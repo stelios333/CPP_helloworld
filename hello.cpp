8dfc5e15986cdfff7e54318e8cd2f4ecf4713b6f
Stelios333 <parasirakisst@outlook.com.gr>
2024-03-31 16:06:53 +0300

    Add variables.cpp

--- hello.cpp
+++ hello.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main() {
+  // Test comment
   cout << "Hello World!\n";
   cout << "I am learing C++!\n";
   return 0;
---
+++ variables.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  // Variable types
+  int myNIntegerum = 5;               // Integer (whole number without decimals)
+  double myFloatNum = 5.99;    // Floating point number (with decimals)
+  char myLetter = 'D';         // Character
+  string myText = "Hello";     // String (text)
+  bool myBoolean = true;   
+  int myNum = 15;
+  myNum = 25;
+  cout << myNum << "\n";
+  cout << "I am " << myNum << "years old\n";
+  // Adding variables together
+  int x = 5;
+  int y = 6;
+  int sum = x + y;
+  cout << sum; 
+  return 0;
+} 
