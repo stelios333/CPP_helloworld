a06dfc7d256e9049017c3e8da58e71dc13de2070
Stelios333 <parasirakisst@outlook.com.gr>
2024-03-31 16:16:31 +0300

    FInish variables lesson

--- variables.cpp
+++ variables.cpp
@@ -16,6 +16,12 @@ int main() {
   int x = 5;
   int y = 6;
   int sum = x + y;
-  cout << sum; 
+  cout << sum << "\n"; 
+  //declare more than one variable of the same type
+  int a = 5, b = 6, z = 50;
+  cout << a + b + z << "\n";
+  // assigning the same value to multiple variables in one line
+  x = y = z = 50;
+  cout << x + y + z;
   return 0;
 } 
