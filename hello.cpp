f6333185b75f0c6857a79a71012d40f93763911f
Stelios333 <parasirakisst@outlook.com.gr>
2024-04-01 19:41:15 +0300

    Build config stuff

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <limits>
 using namespace std;
 
 int main() {
   // Test comment
   cout << "Hello World!\n";
   cout << "I am learing C++!\n";
+  std::cin.clear(); // reset any error flags
+  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // ignore any characters in the input buffer until we find an enter character
+  std::cin.get();
   return 0;
-} 
+}
